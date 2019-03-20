f9e96d670fcdfa891c72c1d7281ee866b158a77f
Sarah Doherty Granoff <sdgranoff@gmail.com>
2019-03-20 17:05:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   square_array = []
   array.each do |numbers|
-    square = numbers*numbers
+    numbers = numbers*numbers
     square_array << square
   end  
 end
